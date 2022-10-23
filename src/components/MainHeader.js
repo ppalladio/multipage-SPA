d6baa466e4d7ed66c7cf9da4d72e07923ff4359b
ppalladio <nine.slide@gmail.com>
2022-10-23 22:59:03 +0200

    NavLink and activeClassName attribute to highlight the current visiting page

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
+import classes from 'MainHeader.module.css'
 const MainHeader = () => {
     return (
-        <header>
+        <header className={classes.header}>
             <nav>
                 <ul>
                     <li>
-                        <Link to="/welcome">welcome</Link>
+                        <NavLink activeClassName={classes.active} to="/welcome">welcome</NavLink>
                     </li>
                     <li>
-                        <Link to="/products">Products</Link>
+                        <NavLink activeClassName={classes.active} to="/products">Products</NavLink>
                     </li>
                 </ul>
             </nav>
