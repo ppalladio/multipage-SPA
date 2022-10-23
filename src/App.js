27c25553a6d629305b87290aa7dd1d3c55821988
ppalladio <nine.slide@gmail.com>
2022-10-23 17:57:22 +0200

    simple routing set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route } from 'react-router-dom';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <>
+            <Route path="/welcome">
+                <Welcome />
+            </Route>
+            <Route path="/products">
+                <Products />
+            </Route>
+        </>
+    );
 }
 
 export default App;
